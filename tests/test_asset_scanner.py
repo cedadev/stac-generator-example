bfb92f6b12d3ae5c1b3112ae57e732239a531745
Mahir-Sparkess <mahirrahman2000@gmail.com>
2021-12-02 12:11:05 +0000

    match regex with aggregation facet

--- tests/test_asset_scanner.py
+++ tests/test_asset_scanner.py
@@ -3,6 +3,11 @@ import os
 
 from asset_scanner.scripts import asset_scanner
 
+# debug purposes: switch to root directory if run as script
+cwd = os.getcwd()
+if cwd.split('/')[-1] == 'tests':
+    os.chdir('..')
+
 extract_assets_conf = os.path.join('conf', 'extract-assets.yaml')
 extract_items_conf = os.path.join('conf', 'extract-items.yaml')
 
@@ -44,3 +49,16 @@ def test_extract_items(capsys):
     extract = ast.literal_eval(output[0])
 
     assert extract['body']['properties']
+
+
+def test_id_generation(capsys):
+    """
+    Test the item_id and file_id are separate ids
+    """
+    asset_scanner_extractor(extract_items_conf)
+    out, err = capsys.readouterr()
+    output = out.split('\n')
+    item = ast.literal_eval(output[0])
+    file = ast.literal_eval(output[1])
+
+    assert item['id'] != file['id']
